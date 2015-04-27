cb78de68e029c28894646320d3cd44edc643740c
Jimmy Ong <gnoymmij@gmail.com>
2015-04-27 14:52:56 +0800

    added ApacheBench

---
+++ ab/README.md
@@ -0,0 +1,14 @@
+#Docker ApacheBench
+
+Containerized ApacheBench.
+
+###Usage
+See bensh.sh for sample usage. Example:
+``` 
+$ bash bench.sh http://www.example.com/
+```
+Or use Compose:
+```
+$ docker-compose up
+```
+Remember to change the necessary parameters if using Compose.
---
+++ ab/bench.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+usage="Usage: $0 url"
+
+# if no args specified, show usage
+[ $# -eq 0 ] && { echo $usage; exit 1; }
+
+export BENCH_HOST=$1
+export REQUESTS=5000
+export CONCURRENCY=100
+
+docker build -t jimmy/ab .
+docker run --rm jimmy/ab ab -k -n $REQUESTS -c $CONCURRENCY $BENCH_HOST
