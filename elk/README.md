fe410dcbab63cd2aa3a43602030cddbc4704c063
Jimmy Ong <gnoymmij@gmail.com>
2015-05-05 23:20:48 +0800

    added ELK stack

---
+++ elk/README.md
@@ -0,0 +1,2 @@
+# elk
+[Elasticsearch](https://www.elastic.co/products/elasticsearch), [Logstash](https://www.elastic.co/products/logstash) and [Kibana](https://www.elastic.co/products/kibana).
---
+++ elk/run.sh
@@ -0,0 +1,3 @@
+docker run --rm -ti -p 9200:9200 -p 9300:9300 --name es gnoymmij/elasticsearch
+docker run --rm -ti -p 5000:5000 -p 5000:5000/udp --link es:elasticsearch gnoymmij/logstash
+docker run --rm -ti -p 80:80 gnoymmoj/kibana
