4702d7e4d1e3cf73056e39234a99bd17615c4af8
Jimmy Ong <gnoymmij@gmail.com>
2015-05-07 17:09:25 +0800

    udpated README.md

--- elk/README.md
+++ elk/README.md
@@ -1,2 +1,5 @@
 # elk
 [Elasticsearch](https://www.elastic.co/products/elasticsearch), [Logstash](https://www.elastic.co/products/logstash) and [Kibana](https://www.elastic.co/products/kibana).
+
+# Usage
+Some issues with using Compose as opposed to good old docker run. Main issue is with logspout.
--- elk/run.sh
+++ elk/run.sh
@@ -1,3 +1,5 @@
 docker run --rm -ti -p 9200:9200 -p 9300:9300 --name es gnoymmij/elasticsearch
 docker run --rm -ti -p 5000:5000 -p 5000:5000/udp --link es:elasticsearch gnoymmij/logstash
 docker run --rm -ti -p 80:80 gnoymmij/kibana
+
+docker run --rm -v /var/run/docker.sock:/tmp/docker.sock gliderlabs/logspout syslog://192.168.1.100:5000
